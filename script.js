dfe8c875a98fba2b85971825809d295808cd5ee2
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-02-28 02:12:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let squares = document.querySelectorAll('.square');
+
+squares.forEach(box=>{
+	box.addEventListener('mouseover',(e)=>{
+		squares.forEach(boxes=>boxes.style.backgroundColor='#6F4E37')
+		box.style.backgroundColor='#E6E6FA'; 
+	})
+	
+});
